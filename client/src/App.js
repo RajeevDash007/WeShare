3cbb226c20a2779a74500b78da89be46ec6fe6e3
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-04-04 02:50:30 +0530

    adding functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-
+import {useRef,useState} from 'react';
 import './App.css';
 
 function App() {
+  const fileInputRef = useRef();
+  const onUploadClick = () =>{
+    fileInputRef.current.click();
+  }
+
+  const [file, setFile] = useState('');
+
+  console.log(file);
+
   return (
     <div className="App">
-        <div class="noise-bg" aria-hidden="true">
+        <div className="noise-bg" aria-hidden="true">
           </div>
-          <div class="shadow"></div>
+          <div className="shadow"></div>
           <svg>
             <filter id="noise-bg-fx">
               <feTurbulence baseFrequency="0.3" />
             </filter>
           </svg>
-      <div class="card">
+      <div className="card">
         <div className="heading">
         WeShare
         </div>
         <div className="UploadWrapper">
             Share your file Online
+        </div>
+        <div className="uploadButton">
+          <button onClick={()=> onUploadClick()}>Upload</button>
+          <input type="file"
+            ref={fileInputRef}
+            style={{display: 'none'}}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
         </div>
       </div>
       
