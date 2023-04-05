fc987b287b8ce02946aa8939aab5b7b1d2ec93f4
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-04-05 11:44:47 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import router from "./routes/routes.js";
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use('/',router);
 
 const PORT = 8000;
