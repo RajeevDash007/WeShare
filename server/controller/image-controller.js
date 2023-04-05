e3cba6f9f49daa6216f57fdc8ed8d0f33a2d14ad
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-04-05 12:34:37 +0530

    handled fileObj

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,11 +1,15 @@
-export const uploadImage = (request,response) => {
+import File from "../models/file.js";
+
+export const uploadImage = async(request,response) => {
     const fileObj = {
         path: request.file.path,
         name: request.file.originalname
     }
    try {
-    
+        const file = await File.create(fileObj);
+        response.status(200).json({path:`http://localhost:8000/file/${file._id}`})
    } catch (error) {
     console.error(error.message);
+    response.status(500).json({error: error.message});
    }
 }
